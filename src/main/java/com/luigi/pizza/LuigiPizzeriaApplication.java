8dc84a43d7672544bf584f2e9230667451f86936
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2026-01-07 10:10:56 -0500

    feat: Commit Inicial

---
+++ src/main/java/com/luigi/pizza/LuigiPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.luigi.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigiPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuigiPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luigi/pizza/LuigiPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigi.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigiPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
