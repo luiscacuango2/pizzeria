31c660eb954885ce5afcb2142fd6efebe339a683
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2026-01-10 05:28:22 -0500

    feat: Configuración de CORS en Spring Security para APIs y Frontend. CorsConfig.java

---
+++ src/main/java/com/luigi/pizza/web/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.luigi.pizza.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // Orígenes permitidos (URL de tu frontend de Vite)
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:5173"));
+        // Métodos permitidos
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // Headers permitidos *
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
--- src/main/java/com/luigi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/luigi/pizza/web/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -16,6 +21,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 // 1. Configuración de Autorización
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/pizzas/**")
                         .authenticated() // Cambiar a permitAll para probar
                         .anyRequest()
