180af03b0585c6e97615a9082e3dd55e60350f75
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2026-01-10 05:39:27 -0500

    feat: Configuración de Reglas de Acceso en Spring Security. requestMatchers

--- src/main/java/com/luigi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/luigi/pizza/web/config/SecurityConfig.java
@@ -2,14 +2,10 @@ package com.luigi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -21,9 +17,8 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 // 1. Configuración de Autorización
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/api/pizzas/**")
-                        .authenticated() // Cambiar a permitAll para probar
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/**").denyAll()
                         .anyRequest()
                         .authenticated()
                 )
