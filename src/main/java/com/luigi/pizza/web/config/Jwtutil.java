364d4ad3c6964ed2403ba12730fd74d3f6956ca5
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2026-01-11 04:18:29 -0500

    feat: Creación y Uso de JSON Web Tokens en Java con Auth0. Jwtutil

---
+++ src/main/java/com/luigi/pizza/web/config/Jwtutil.java
@@ -0,0 +1,23 @@
+package com.luigi.pizza.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class Jwtutil {
+    private static String SECRET_KEY = "luigi123";
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("pizzeria")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
