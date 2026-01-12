44c9f8c27343bcd03ddb746bfb46fe0263231526
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2026-01-12 05:08:43 -0500

    feat: Auditoría de Usuarios con Spring Security y Data JPA. @CreatedBy, @LastModifiedBy y AuditUsername

--- src/main/java/com/luigi/pizza/persistence/audit/AuditConfig.java
+++ src/main/java/com/luigi/pizza/persistence/audit/AuditConfig.java
@@ -1,21 +1,10 @@
 package com.luigi.pizza.persistence.audit;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 @Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+// Apuntamos al nombre del componente AuditUsername
+@EnableJpaAuditing(auditorAwareRef = "auditUsername")
 public class AuditConfig {
-
-    @Bean
-    public AuditorAware<String> auditorProvider() {
-        // En un futuro, aquí es donde obtendremos el usuario de Spring Security
-        // return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
-
-        return () -> Optional.of("ADMIN_PIZZERIA");
-    }
 }
